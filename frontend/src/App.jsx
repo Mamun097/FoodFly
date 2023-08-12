d89c6b39f16979e3f2b390c8d3042033ae01743d
mamun097 <mamunmunshi055@gmail.com>
2023-08-12 18:51:33 +0600

    restaurant pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import SignIn from "./Auth/SignIn";
 import SignUp from "./Auth/SignUp";
 import HomePage_Guest from "./Guest/HomePage_Guest";
+import HomePage_Restaurant from "./Restaurant/HomePage_Restaurant";
 
 function App() {
   return (
     <div>
-      {/* <SignIn />
-      <SignUp/> */}
-      <HomePage_Guest/>
+      {/* <SignIn /> */}
+      {/* <SignUp/> */}
+      {/* <HomePage_Guest/> */}
+      <HomePage_Restaurant/>
     </div> 
   );
 }
